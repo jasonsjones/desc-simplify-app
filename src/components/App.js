eb05b0c296435f17d0a11a65d1f39dc2d53b2c63
Jason Jones <jsjones96@gmail.com>
2018-09-26 08:16:13 -0700

    Wire up simple data fetching from api

--- src/components/App.js
+++ src/components/App.js
@@ -7,22 +7,64 @@ import SignupPage from './SignupPage';
 import NotFoundPage from './NotFoundPage';
 import './App.css';
 
-const AppContent = () => {
-    return (
-        <div>
-            <header className="grey lighten-4">
-                <div>
-                    <h1
-                        className="grey-text center-align"
-                        style={{ margin: '0', padding: '30px 0 0 0' }}
-                    >
-                        DESC Simplify Project
-                    </h1>
-                </div>
-            </header>
-        </div>
-    );
-};
+class AppContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDataFetched: false,
+            message: '',
+            version: ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    isDataFetched: true,
+                    message: data.name,
+                    version: data.version
+                });
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="grey lighten-4">
+                    <div>
+                        <h1
+                            className="grey-text center-align"
+                            style={{ margin: '0', padding: '30px 0 0 0' }}
+                        >
+                            DESC Simplify Project
+                        </h1>
+                        {this.state.isDataFetched && (
+                            <div className="container" style={{ marginTop: '75px' }}>
+                                <div className="row">
+                                    <div className="col s12 l6 offset-l3">
+                                        <div className="card-panel grey lighten-3">
+                                            <h5 className="teal-text center-align">
+                                                Data from API...
+                                            </h5>
+                                            <hr />
+                                            <h6 className="ds-m-top_medium">
+                                                Name: {this.state.message}
+                                            </h6>
+                                            <h6>Version: {this.state.version}</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
 
 class App extends React.Component {
     render() {

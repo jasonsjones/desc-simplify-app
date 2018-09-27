22ee6ed2e8660e4e683f263ad1e19cd419264c85
Jason Jones <jsjones96@gmail.com>
2018-09-27 14:34:09 -0700

    Add mock data

---
+++ src/components/__mocks__/dataMock.js
@@ -0,0 +1,21 @@
+export const jsonResponse = {
+    success: true,
+    message: 'user authenticated',
+    payload: {
+        user: {
+            name: { first: 'Oliver', last: 'Queen' },
+            lastLoginAt: '2018-09-26T16:00:05.268Z',
+            isEmailVerified: false,
+            roles: ['admin', 'approver'],
+            _id: '5b75f1d0c6b29e1274758754',
+            email: 'oliver@qc.com',
+            password: '$2a$12$wtaoUXImI2GR1i6TY/7nPew2C8FzffowFndWgvQRcdIqtKFrGxmwi',
+            createdAt: '2018-08-16T21:51:12.813Z',
+            updatedAt: '2018-09-26T16:00:05.269Z',
+            passwordLastUpdatedAt: '2018-08-16T21:51:13.471Z',
+            __v: 0
+        },
+        token:
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1Yjc1ZjFkMGM2YjI5ZTEyNzQ3NTg3NTQiLCJlbWFpbCI6Im9saXZlckBxYy5jb20iLCJpYXQiOjE1Mzc5Nzc2MDUsImV4cCI6MTUzODA2NDAwNX0.eoGaRIH_h7fgyV7W_oqsaGLuPrpC-Lm2shbE6ymW2TU'
+    }
+};

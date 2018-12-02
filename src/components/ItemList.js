8309e9d931668aa0f26242f169eac572c9868581
Jason Jones <jsjones96@gmail.com>
2018-12-01 19:07:11 -0800

    refactor: add Item component w/in ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,38 +1,60 @@
 import React from 'react';
 
+const items = [
+    {
+        id: 1,
+        name: {
+            firstName: 'Henry',
+            lastName: 'Smith'
+        },
+        quantity: 3,
+        item: 'plates',
+        date: 'May 24'
+    },
+    {
+        id: 2,
+        name: {
+            firstName: 'Sharon',
+            lastName: 'Smith'
+        },
+        quantity: 2,
+        item: 'pants',
+        date: 'May 20'
+    },
+    {
+        id: 3,
+        name: {
+            firstName: 'Sharon',
+            lastName: 'Smith'
+        },
+        quantity: 1,
+        item: 'pillow',
+        date: 'May 20'
+    }
+];
+
+const Item = props => (
+    <div className="row">
+        <div className="col s1 m4">
+            {props.name.firstName} {props.name.lastName}
+        </div>
+        <div className="col s1 m4">
+            {props.quantity} {props.item}
+        </div>
+        <div className="col s1 m4 right-align">{props.date}</div>
+    </div>
+);
+
 const ItemList = props => (
     <ul className="collection with-header">
         <li className="collection-header">
             <h5>{props.title}</h5>
         </li>
-        <li className="collection-item">
-            <div className="row">
-                <div className="col s1 m4">Sharon Smith</div>
-                <div className="col s1 m4">2 pairs of pants</div>
-                <div className="col s1 m4 right-align">May 20</div>
-            </div>
-        </li>
-        <li className="collection-item">
-            <div className="row">
-                <div className="col s1 m4">Sharon Smith</div>
-                <div className="col s1 m4">4 plates</div>
-                <div className="col s1 m4 right-align">May 21</div>
-            </div>
-        </li>
-        <li className="collection-item">
-            <div className="row">
-                <div className="col s1 m4">Sharon Smith</div>
-                <div className="col s1 m4">2 pairs of pants</div>
-                <div className="col s1 m4 right-align">May 20</div>
-            </div>
-        </li>
-        <li className="collection-item">
-            <div className="row">
-                <div className="col s1 m4">Sharon Smith</div>
-                <div className="col s1 m4">2 pairs of pants</div>
-                <div className="col s1 m4 right-align">May 20</div>
-            </div>
-        </li>
+        {items.map(item => (
+            <li key={item.id} className="collection-item">
+                <Item {...item} />
+            </li>
+        ))}
     </ul>
 );
 

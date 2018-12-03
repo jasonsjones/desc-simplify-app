9803eb633afbe645bd5d3d06c0f7cc2ee11f0246
Jason Jones <jsjones96@gmail.com>
2018-12-03 10:35:52 -0800

    refactor: Change Navbar to use React Material UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,37 +2,83 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+import * as actions from '../actions/actions';
 import SignedOutLinks from './SignedOutLinks';
 import SignedInLinks from './SignedInLinks';
 
-const Navbar = props => {
-    return (
-        <nav className="nav-wrapper teal">
-            <div className="container">
-                <Link to="/" className="brand-logo">
-                    DESC Simplify
-                </Link>
-                {props.isAuth ? (
-                    <SignedInLinks contextUser={props.contextUser} />
-                ) : (
-                    <SignedOutLinks />
-                )}
-            </div>
-        </nav>
-    );
+const HomeLink = props => <Link to="/" style={{ textDecoration: 'none' }} {...props} />;
+
+const styles = {
+    root: {
+        flexGrow: 1
+    },
+    grow: {
+        flexGrow: 1
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    }
 };
 
-Navbar.propTypes = {
+class MenuAppBar extends React.Component {
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            className={classes.menuButton}
+                            color="inherit"
+                            aria-label="Menu"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            component={HomeLink}
+                            variant="h6"
+                            color="inherit"
+                            className={classes.grow}
+                        >
+                            DESC In Kind Portal
+                        </Typography>
+                        {this.props.isAuth ? <SignedInLinks /> : <SignedOutLinks />}
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+MenuAppBar.propTypes = {
     isAuth: PropTypes.bool,
-    contextUser: PropTypes.object
+    classes: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => {
     return {
-        isAuth: state.isAuth,
-        contextUser: state.contextUser
+        isAuth: state.isAuth
     };
 };
 
-export default connect(mapStateToProps)(Navbar);
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(actions.userLogout())
+    };
+};
+
+export default withStyles(styles)(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(MenuAppBar)
+);

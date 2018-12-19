6e4e00773656113b98720c8ca7a6b5f44a6dfa63
Jason Jones <jsjones96@gmail.com>
2018-12-19 10:57:10 -0800

    refactor: componentize ItemExpansion a bit more

--- src/components/ItemExpansion.js
+++ src/components/ItemExpansion.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
     Typography,
-    Grid,
     ExpansionPanel,
     ExpansionPanelSummary,
     ExpansionPanelDetails
@@ -9,23 +8,12 @@ import {
 import { withStyles } from '@material-ui/core/styles';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const ItemSummaryHeader = props => (
-    <Grid container justify="space-between">
-        <Grid item>
-            <Typography>{`
-                ${props.submittedBy.name.first} ${props.submittedBy.name.last}
-            `}</Typography>
-        </Grid>
-        <Grid item>
-            <Typography>{`${props.numberOfItems} ${props.name}`}</Typography>
-        </Grid>
-        <Grid item>
-            <Typography>{new Date(props.createdAt).toDateString()}</Typography>
-        </Grid>
-    </Grid>
-);
-
 const styles = () => ({
+    summaryContainer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
     panelDetails: {
         alignItems: 'center'
     },
@@ -34,34 +22,60 @@ const styles = () => ({
     }
 });
 
+const _ItemSummary = props => {
+    const { classes } = props;
+    return (
+        <div className={classes.summaryContainer}>
+            <Typography>{`
+                ${props.submittedBy.name.first} ${props.submittedBy.name.last}
+            `}</Typography>
+            <Typography>{`${props.numberOfItems} ${props.name}`}</Typography>
+            <Typography>{new Date(props.createdAt).toDateString()}</Typography>
+        </div>
+    );
+};
+
+const ItemSummary = withStyles(styles)(_ItemSummary);
+
+const _PanelDetails = props => {
+    const { classes } = props;
+    return (
+        <React.Fragment>
+            <Typography variant="body2" className={classes.spacingRight}>
+                This will contain details about the request...
+            </Typography>
+            <Typography variant="body2" className={classes.spacingRight}>
+                Urgency:
+            </Typography>
+            <Typography variant="body1" className={classes.spacingRight}>
+                {props.urgency}
+            </Typography>
+            {props.notes.length > 0 && (
+                <Typography variant="body2" className={classes.spacingRight}>
+                    NOTES:
+                </Typography>
+            )}
+            {props.notes &&
+                props.notes.map(note => (
+                    <Typography key={note._id} variant="body1">
+                        {note.body}
+                    </Typography>
+                ))}
+        </React.Fragment>
+    );
+};
+
+const PanelDetails = withStyles(styles)(_PanelDetails);
+
 const ItemExpansion = props => {
     const { classes } = props;
     return (
         <ExpansionPanel elevation={0}>
             <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <ItemSummaryHeader {...props} />
+                <ItemSummary {...props} />
             </ExpansionPanelSummary>
             <ExpansionPanelDetails className={classes.panelDetails}>
-                <Typography variant="body2" className={classes.spacingRight}>
-                    This will contain details about the request...
-                </Typography>
-                <Typography variant="body2" className={classes.spacingRight}>
-                    Urgency:
-                </Typography>
-                <Typography variant="body1" className={classes.spacingRight}>
-                    {props.urgency}
-                </Typography>
-                {props.notes.length > 0 && (
-                    <Typography variant="body2" className={classes.spacingRight}>
-                        NOTES:
-                    </Typography>
-                )}
-                {props.notes &&
-                    props.notes.map(note => (
-                        <Typography key={note._id} variant="body1">
-                            {note.body}
-                        </Typography>
-                    ))}
+                <PanelDetails {...props} />
             </ExpansionPanelDetails>
         </ExpansionPanel>
     );
